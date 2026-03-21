0f8e12d8e78036ce1f7679f0b0eb6b13343b8916
BharathKumarReddy <nbkumar2103@gmail.com>
2026-03-21 16:36:17 +0530

    updated file

--- event-bridge.tf
+++ event-bridge.tf
@@ -4,17 +4,17 @@ resource "aws_cloudwatch_event_rule" "ec2_auto_stop_rule" {
   schedule_expression = "cron(52 10 ? * MON-FRI *)" # 8 PM UTC, Monday to Friday
 }
 
-# resource "aws_cloudwatch_event_rule" "ec2_auto_start_rule" {
-#   name                = "ec2-auto-start-weekdays-8am"
-#   description         = "Triggers EC2 auto-start at 8 AM on weekdays"
-#   schedule_expression = "cron(43 13 ? * MON-FRI *)" # 8 AM UTC, Monday to Friday
-# }
+resource "aws_cloudwatch_event_rule" "ec2_auto_start_rule" {
+  name                = "ec2-auto-start-weekdays-8am"
+  description         = "Triggers EC2 auto-start at 8 AM on weekdays"
+  schedule_expression = "cron(10 11 ? * MON-FRI *)" # 8 AM UTC, Monday to Friday
+}
 
-# resource "aws_cloudwatch_event_target" "ec2_auto_start_target" {
-#   rule      = aws_cloudwatch_event_rule.ec2_auto_start_rule.name
-#   target_id = "EC2AutoStartLambda"
-#   arn       = aws_lambda_function.ec2_auto_start.arn
-# }
+resource "aws_cloudwatch_event_target" "ec2_auto_start_target" {
+  rule      = aws_cloudwatch_event_rule.ec2_auto_start_rule.name
+  target_id = "EC2AutoStartLambda"
+  arn       = aws_lambda_function.ec2_auto_start.arn
+}
 
 resource "aws_cloudwatch_event_target" "ec2_auto_stop_target" {
   rule      = aws_cloudwatch_event_rule.ec2_auto_stop_rule.name
@@ -22,13 +22,13 @@ resource "aws_cloudwatch_event_target" "ec2_auto_stop_target" {
   arn       = aws_lambda_function.ec2_auto_stop.arn
 }
 
-# resource "aws_lambda_permission" "allow_cloudwatch_to_call_ec2_auto_start" {
-#   statement_id  = "AllowExecutionFromCloudWatch"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.ec2_auto_start.function_name
-#   principal     = "events.amazonaws.com"
-#   source_arn    = aws_cloudwatch_event_rule.ec2_auto_start_rule.arn
-# }
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_ec2_auto_start" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ec2_auto_start.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ec2_auto_start_rule.arn
+}
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_ec2_auto_stop" {
   statement_id  = "AllowExecutionFromCloudWatch"
